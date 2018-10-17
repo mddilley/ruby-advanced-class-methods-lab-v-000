f600a0d839a5695261b8c8e03915d0d677e36707
Mike Dilley <mike.dilley@gmail.com>
2018-10-17 16:25:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    self.find_by_name(name) == false ? find_by_name(name) : create_by_name(name)
+    self.find_by_name(name) == false ? self.find_by_name(name) : self.create_by_name(name)
   end
 
 end
